35e2d7eee03d9143b287cf5e4e959e8b2e9bbf1c
Jakub Stejskal <jakub@jakub-stejskal.cz>
2019-01-16 16:11:29 +0100

    Edit sort function Add .env to gitignore

--- components/Stats.js
+++ components/Stats.js
@@ -1,33 +1,33 @@
 import React from "react"
 import { connect } from 'react-redux'
+
 // import * as firebase from 'firebase';
 
 class Stats extends React.Component {
-    
-    getFormattedStatisticsArray(statistics) {
+
+    getFormattedStatisticsArray (statistics) {
         let parsedArray = {}
         statistics.forEach((child, key) => {
-            var childData = child.val()
-            var name = child.key
-            parsedArray[name] = []
-            for(var record_id in childData) {
-                let child = childData[record_id]
-                parsedArray[name].push(child)
+            var childData       = child.val()
+            var name            = child.key
+            parsedArray[ name ] = []
+            for (var record_id in childData) {
+                let child = childData[ record_id ]
+                parsedArray[ name ].push(child)
             }
         })
         return parsedArray
     }
 
-    getFormatted(timer) {
+    getFormatted (timer) {
         let date = new Date(null)
         date.setSeconds(timer)
-        return date.toISOString().substr(11,8).split(':');
+        return date.toISOString().substr(11, 8).split(':');
     }
 
-    render() {
-        const { statistics} = this.props
-        let stats = this.getFormattedStatisticsArray(statistics)      
-        
+    render () {
+        const {statistics} = this.props
+        let stats          = this.getFormattedStatisticsArray(statistics)
 
         var scoresByPerson = {}
 
@@ -35,100 +35,107 @@ class Stats extends React.Component {
         var uniqueDates = []
         Object.keys(stats).map((name, key) => {
 
-            scoresByPerson[name] = []
+            scoresByPerson[ name ] = []
 
-            stats[name].map((data) => {
-                if(uniqueDates.indexOf(data.date) == -1) {
+            stats[ name ].map((data) => {
+                if (uniqueDates.indexOf(data.date) == -1) {
                     uniqueDates.push(data.date)
                 }
 
-                scoresByPerson[name].push(data.seconds)
+                scoresByPerson[ name ].push(data.seconds)
 
             })
 
 
         })
 
-
         uniqueDates.sort((a, b) => {
-            return new Date(b).getTime() - new Date(a).getTime()
+            if (a > b) return -1;
+            if (a < b) return 1;
+            return 0;
+            //return new Date(b).getTime() - new Date(a).getTime()
         })
 
-        var parsedTableArray = {}
+        var parsedTableArray   = {}
         var highestScoreByDate = {}
         uniqueDates.map((date, key) => {
-            parsedTableArray[date] = []
-            highestScoreByDate[date] = 0
+            parsedTableArray[ date ]   = []
+            highestScoreByDate[ date ] = 0
             Object.keys(stats).map((name, key) => {
-                let scores = stats[name]
+                let scores = stats[ name ]
 
                 scores.filter((data, key) => {
-                    if(data.date == date) {
+                    if (data.date == date) {
                         return true
                     }
                 }).map((data) => {
-                    parsedTableArray[date][name] = data.seconds
-                    if(data.seconds > highestScoreByDate[date]) {
-                        highestScoreByDate[date] = data.seconds
+                    parsedTableArray[ date ][ name ] = data.seconds
+                    if (data.seconds > highestScoreByDate[ date ]) {
+                        highestScoreByDate[ date ] = data.seconds
                     }
                 })
 
             })
         })
-  
+        //console.log(parsedTableArray, scoresByPerson, stats)
         return (
             <div>
                 <table className="statistics">
                     <thead>
-                        <tr>
-                            <th></th>
-                            {
-                                Object.keys(stats).map((name, key) => {
-                                    return <th key={key}>{name}</th>        
-                                })
-                            }  
-                        </tr>
+                    <tr>
+                        <th></th>
+                        {
+                            Object.keys(stats).map((name, key) => {
+                                return <th key={key}>{name}</th>
+                            })
+                        }
+                    </tr>
 
-                        <tr>
-                            <td>Record</td>
-                            {
-                                Object.keys(scoresByPerson).map((name, key) => {
-                                    let seconds = this.getFormatted(Math.max(...scoresByPerson[name])) 
-                                    return <td key={key}><span className="personal-record">{`${seconds[1]}:${seconds[2]}`}</span></td>
-                                })
-                            }
-                        </tr>
+                    <tr>
+                        <td>Record</td>
+                        {
+                            Object.keys(scoresByPerson).map((name, key) => {
+                                let seconds = this.getFormatted(Math.max(...scoresByPerson[ name ]))
+                                return <td key={key}><span
+                                    className="personal-record">{`${seconds[ 1 ]}:${seconds[ 2 ]}`}</span></td>
+                            })
+                        }
+                    </tr>
 
                     </thead>
                     <tbody>
-                        {
-                            uniqueDates.map((date, key) => {
-                                return (
-                                    <tr key={key}>
-                                        <td>{date}</td>
-                                        {
-
-                                            Object.keys(stats).map((name, key) => {
-                                                let seconds = parsedTableArray[date][name]                                               
-                                                if(typeof seconds == 'undefined' || parseInt(seconds) === 0 || seconds == '') {
-                                                    return (
-                                                        <td key={key}>--</td>
-                                                    )
-                                                }else {
-                                                    let score = this.getFormatted(seconds)                                                   
-                                                    return (
-                                                        <td key={key} className={ highestScoreByDate[date] == seconds ? 'daily-winner' : '' }>
-                                                            <span>{score[1]}</span>
-                                                            <span>{score[2]}</span>
-                                                        </td>
-                                                    )
-                                                }
-                                            })
-                                        }
-                                    </tr>
-                                )
-                            })
-                        }
+                    {
+                        uniqueDates.map((date, key) => {
+                            return (
+                                <tr key={key}>
+                                    <td>{date}</td>
+                                    {
+                                        Object.keys(stats).map((name, key) => {
+                                            let seconds = parsedTableArray[ date ][ name ]
+                                            //console.log(seconds, date, name)
+                                            if (typeof seconds == 'undefined' || parseInt(seconds) === 0 || seconds == '') {
+                                                return (
+                                                    <td key={key}>--</td>
+                                                )
+                                            } else {
+                                                let score = this.getFormatted(seconds)
+                                                return (
+                                                    <td key={key}
+                                                        className={highestScoreByDate[ date ] == seconds ? 'daily-winner' : ''}>
+                                                        <span>{score[ 1 ]}</span>
+                                                        <span>{score[ 2 ]}</span>
+                                                        <br/>
+                                                        <span>{score[ 1 ]}</span>
+                                                        <span>{score[ 2 ]}</span>
+                                                    </td>
+                                                )
+                                            }
+                                        })
+                                    }
+                                </tr>
+                            )
+                        })
+                    }
                     </tbody>
                 </table>
             </div>
